7a5d57f6c0a6e52f7a4cb5aa4689996333e4b4a0
Nkwenti <fonnkwenti85@gmail.com>
2021-08-31 10:45:37 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_instance" "my-first-server" {
+  ami           = "ami-021809d9177640a20"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
